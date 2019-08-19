3bbc620aa0538d28011a79c3af46d61d5c6d1db9
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-08-19 22:42:16 +0000

    added grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,47 @@ module.exports = function(grunt) {
 // Do grunt-related things in here
 
 // grunt configs
-        grunt.initConfig({
+
 
 // SOPHIE STUFF HERE
 ///////////////////////////////////
 // JOHN STUFF HERE
+    grunt.initConfig({
+        csslint: {
+          strict: {
+            options: {
+              import: 2
+            },
+            src: ['css/*.css']
+          },
+          lax: {
+            options: {
+              import: false
+            },
+            src: ['css/*.css']
+          }
+        },
+        cssmin: {
+          target: {
+            files: [{
+              expand: true,
+              cwd: 'css/',
+              src: ['*.css', '!*.min.css'],
+              dest: 'css/',
+              ext: '.min.css'
+            }]
+          }
+        }
+    });
 
-        });
+    // load plugins here
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-        // load plugins here
 
-
-        // register tasks here
+    // register tasks here
+    grunt.registerTask('lintCSS', ['csslint']);
+    grunt.registerTask('minCSS', ['cssmin']);
+    // combining two tasks in one
+    grunt.registerTask('checkCSS', ['csslint', 'cssmin']);
 }
