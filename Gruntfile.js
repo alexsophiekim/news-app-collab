859256cf1c86f75a20f21bd889f0851da6bed02e
John Bretherton <270022735@bun2-206-10.local>
2019-08-20 09:15:56 +1200

    grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+// Do grunt-related things in here
+
+// grunt configs
+        grunt.initConfig({
+
+// SOPHIE STUFF HERE
+///////////////////////////////////
+// JOHN STUFF HERE
+
+        });
+
+        // load plugins here
+
+
+        // register tasks here
+}
