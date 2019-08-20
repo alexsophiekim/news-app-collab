6a18bd38e5ee7cc5a885339b505d02d3b4fbabc2
John Bretherton <270022735@bun2-206-10.local>
2019-08-21 10:02:30 +1200

    set up config.json, wrote js, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # news-app-collab
+# A News API project by Sophie and John 
+
+[See News API](https://newsapi.org/)
+
+# Installation
+
+To install everything needed for this project, you need to have a version of **Node JS** and **npm** installed on your machine.
+
+In Terminal on your computer:
+```$ git clone https://github.com/ThisIsMattSaunders/formative3.1.git
+```
+
+Still in Terminal, but now in your local server (vagrant, localhost, etc.):
+```$ npm install
+```
+
+This will install Bootstrap, jQuery, Popper.js, and Grunt (which are listed in the package.json file) when you type **npm install** in the Terminal.
+
+You also need to create a config.json file in your root directory and paste the following into the file. You need to get a News API Key from the News API website (see link above) and then include it in the config.json file.
+
+Here's how it should look:
+
+```{
+   "newsAPIkey": "ADD_API_KEY_HERE"
+    }
+```
--- js/script.js
+++ js/script.js
@@ -1,3 +1,105 @@
-$(document).ready(function(){
-  console.log('Something different ');
+
+let newsBox = document.getElementById('NewsBox');
+// On click, pull local (NZ) news headlines
+$('#localButton').click(function(){
+ console.log('local button clicked');
+    var key;
+    $.ajax({
+        url: "config.json",
+        dataType: "json",
+        type: "GET",
+        success:function(data){
+            key = data[0].NEWS_API_KEY; //key from config.json
+            // console.log(key);
+            getData(key);
+        },
+        error:function(error){
+            console.log("ERROR");
+            console.log(error);
+        }
+    });
+    function getData(key){
+        console.log(key);
+        $.ajax({
+            url: 'https://newsapi.org/v2/top-headlines?' +
+              'country=nz&' +
+              'pageSize=12&' +
+              'apiKey=' + key,
+            dataType: "json",
+            type: "GET",
+            success:function(newsData){
+                dataJSON = newsData;
+                console.log(dataJSON);
+                // console.log(newsData.articles.length);
+                for (var i = 0; i < newsData.articles.length; i++) {
+                    newsBox.innerHTML +=
+                    `<div class="card" style="width: 20rem; margin: 25px">
+                          <div class="card-body">
+                            <h5 class="card-title">${newsData.articles[i].title}</h5>
+                            <p class="card-text">${newsData.articles[i].description}</p>
+                            <a href="${newsData.articles[i].url}" class="btn btn-secondary">Read more</a>
+                          </div>
+                         </div>`;
+                }
+            },
+            error:function(error){
+                console.log("Error");
+            }
+
+        });
+
+    }
+
 });
+
+// On click, pull world headlines
+$('#worldButton').click(function(){
+
+    var key;
+    $.ajax({
+        url: "config.json",
+        dataType: "json",
+        type: "GET",
+        success:function(data){
+            key = data[0].NEWS_API_KEY; //key from config.json
+            console.log(key);
+            getData(key);
+        },
+        error:function(error){
+            console.log("ERROR");
+            console.log(error);
+        }
+    });
+    function getData(key){
+        // console.log(key);
+        $.ajax({
+            url: 'https://newsapi.org/v2/top-headlines?' +
+              'language=en&' +
+              'pageSize=12&' +
+              'apiKey=' + key,
+            dataType: "json",
+            type: "GET",
+            success:function(newsData){
+                dataJSON = newsData;
+                console.log(dataJSON);
+                // console.log(newsData.articles.length);
+                for (var i = 0; i < newsData.articles.length; i++) {
+                    newsBox.innerHTML +=
+                    `<div class="card" style="width: 20rem; margin: 25px">
+                          <div class="card-body">
+                            <h5 class="card-title">${newsData.articles[i].title}</h5>
+                            <p class="card-text">${newsData.articles[i].description}</p>
+                            <a href="${newsData.articles[i].url}" class="btn btn-secondary">Read more</a>
+                          </div>
+                         </div>`;
+                }
+            },
+            error:function(error){
+                console.log("Error");
+            }
+
+        });
+
+    }
+
+})
